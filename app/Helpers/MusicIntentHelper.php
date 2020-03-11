f3e927b84e005ea00d219d1b5603a7048c6c0d01
Huu Nam <namph13@cyberspace.vn>
2020-03-11 13:14:31 +0700

    complete next_sentence

--- app/Helpers/MusicIntentHelper.php
+++ app/Helpers/MusicIntentHelper.php
@@ -8,12 +8,56 @@ class MusicIntentHelper extends IntentHelper
 	public static function process($session, $PID, $page_id, $sentence) {
 		$result = 'bài gì đó...';
 
-		$music = Music::where('content', 'like', '%' . $sentence . '%')->first();
+		$type = 'NAME_SONG';
 
-		if ($music) {
-			$result = $music->name;
+		if ($session->addition != null) {
+			$type = $session->addition;
 		}
 
+		if ($type == 'INFO_SONG') {
+			$music = Music::where('name', 'like', '%' . $sentence . '%')->first();
+
+			if ($music) {
+				$result = $music->name;
+			}
+
+		} else {
+			$music = Music::where('content', 'like', '%' . $sentence . '%')->first();
+
+			if ($music) {
+				
+				if ($type == 'NAME_SONG') {
+					$result = $music->name;
+				}
+				if ($type == 'NEXT_SENTENCE') {
+					$content = $music->content;
+					$content_split = explode("\n", $content);
+
+					$count = 0;
+					$max_sen = count($content_split);
+
+					foreach ($content_split as $sen) {
+
+						$count += 1;
+
+						$check = strpos($sen, $sentence);
+						if ($check !== FALSE) {
+							if (strlen($sen) > strlen($sentence)) {
+								$result = substr($sen, strlen($sentence));
+								break;
+							} else {
+								if ($count < $max_sen) {
+									$result = $content_split[$count];
+								} else {
+									$result = 'Hết';
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
 		return [
 			[
 				'id'	=>	null,
